f20abfa75c38bbf02efdb60cb5521df942d71d2f
cevans@google.com <cevans@google.com>
2012-02-10 02:44:25 +0100

    Merge 107123
    BUG=112694
    Review URL: https://chromiumcodereview.appspot.com/9372049

--- Source/WebCore/dom/Node.cpp
+++ Source/WebCore/dom/Node.cpp
@@ -642,7 +642,7 @@ void Node::normalize()
             continue;
         }
 
-        Text* text = static_cast<Text*>(node.get());
+        RefPtr<Text> text = static_cast<Text*>(node.get());
 
         // Remove empty text nodes.
         if (!text->length()) {
