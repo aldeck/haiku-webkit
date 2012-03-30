215fffcf841bf50cdd4a22115ffa23ddc2c3648c
cevans@google.com <cevans@google.com>
2012-03-30 03:01:22 +0200

    Merge 111601
    BUG=118009
    Review URL: https://chromiumcodereview.appspot.com/9949002

--- Source/WebCore/rendering/svg/RenderSVGRoot.h
+++ Source/WebCore/rendering/svg/RenderSVGRoot.h
@@ -55,6 +55,10 @@ public:
 
     virtual bool hasRelativeDimensions() const;
 
+    // The flag is cleared at the beginning of each layout() pass. Elements then call this
+    // method during layout when they are invalidated by a filter.
+    static void addResourceForClientInvalidation(RenderSVGResourceContainer*);
+
 private:
     virtual RenderObjectChildList* virtualChildren() { return children(); }
     virtual const RenderObjectChildList* virtualChildren() const { return children(); }
@@ -100,6 +104,7 @@ private:
     FloatRect m_repaintBoundingBox;
     mutable AffineTransform m_localToParentTransform;
     AffineTransform m_localToBorderBoxTransform;
+    HashSet<RenderSVGResourceContainer*> m_resourcesNeedingToInvalidateClients;
     bool m_isLayoutSizeChanged : 1;
     bool m_needsBoundariesOrTransformUpdate : 1;
 };
