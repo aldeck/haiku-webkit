00ccbba72677c7ccb478b5e6078b259aa202fa6f
mnaganov@chromium.org <mnaganov@chromium.org>
2012-04-03 19:07:17 +0200

    Revert 103073 - Scroll non-visible edit controls and caret into the center of the view when starting typing.
    https://bugs.webkit.org/show_bug.cgi?id=65027
    
    Reviewed by Ryosuke Niwa.
    
    Tests: editing/input/caret-at-the-edge-of-contenteditable.html
           editing/input/caret-at-the-edge-of-input.html
           editing/input/reveal-caret-of-multiline-contenteditable.html
           editing/input/reveal-caret-of-multiline-input.html
           editing/input/reveal-contenteditable-on-input-vertically.html
           editing/input/reveal-contenteditable-on-paste-vertically.html
           editing/input/reveal-edit-on-input-vertically.html
           editing/input/reveal-edit-on-paste-vertically.html
    
    * editing/Editor.cpp:
    (WebCore::Editor::insertTextWithoutSendingTextEvent):
    (WebCore::Editor::revealSelectionAfterEditingOperation):
    
    * editing/input/caret-at-the-edge-of-contenteditable-expected.png: Added.
    * editing/input/caret-at-the-edge-of-contenteditable-expected.txt: Added.
    * editing/input/caret-at-the-edge-of-contenteditable.html: Added.
    * editing/input/caret-at-the-edge-of-input-expected.png: Added.
    * editing/input/caret-at-the-edge-of-input-expected.txt: Added.
    * editing/input/caret-at-the-edge-of-input.html: Added.
    * editing/input/resources/reveal-utilities.js: Added.
    * editing/input/reveal-caret-of-multiline-contenteditable-expected.png: Added.
    * editing/input/reveal-caret-of-multiline-contenteditable-expected.txt: Added.
    * editing/input/reveal-caret-of-multiline-contenteditable.html: Added.
    * editing/input/reveal-caret-of-multiline-input-expected.png: Added.
    * editing/input/reveal-caret-of-multiline-input-expected.txt: Added.
    * editing/input/reveal-caret-of-multiline-input.html: Added.
    * editing/input/reveal-contenteditable-on-input-vertically-expected.txt: Added.
    * editing/input/reveal-contenteditable-on-input-vertically.html: Added.
    * editing/input/reveal-contenteditable-on-paste-vertically-expected.txt: Added.
    * editing/input/reveal-contenteditable-on-paste-vertically.html: Added.
    * editing/input/reveal-edit-on-input-vertically-expected.txt: Added.
    * editing/input/reveal-edit-on-input-vertically.html: Added.
    * editing/input/reveal-edit-on-paste-vertically-expected.txt: Added.
    * editing/input/reveal-edit-on-paste-vertically.html: Added.
    * platform/chromium/test_expectations.txt: Mark new tests as FAIL to grab results from bots.
    * platform/gtk/test_expectations.txt: Mark new tests as FAIL to grab results from bots.
    * platform/qt/test_expectations.txt: Mark new tests as FAIL to grab results from bots.
    * platform/win/test_expectations.txt: Mark new tests as FAIL to grab results from bots.
    
    TBR=mnaganov@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9965112

--- Source/WebCore/editing/Editor.cpp
+++ Source/WebCore/editing/Editor.cpp
@@ -1000,7 +1000,7 @@ bool Editor::insertTextWithoutSendingTextEvent(const String& text, bool selectIn
             // Reveal the current selection
             if (Frame* editedFrame = document->frame())
                 if (Page* page = editedFrame->page())
-                    page->focusController()->focusedOrMainFrame()->selection()->revealSelection(ScrollAlignment::alignCenterIfNeeded);
+                    page->focusController()->focusedOrMainFrame()->selection()->revealSelection(ScrollAlignment::alignToEdgeIfNeeded);
         }
     }
 
@@ -2339,7 +2339,7 @@ void Editor::revealSelectionAfterEditingOperation()
     if (m_ignoreCompositionSelectionChange)
         return;
 
-    m_frame->selection()->revealSelection(ScrollAlignment::alignCenterIfNeeded);
+    m_frame->selection()->revealSelection(ScrollAlignment::alignToEdgeIfNeeded);
 }
 
 void Editor::setIgnoreCompositionSelectionChange(bool ignore)
